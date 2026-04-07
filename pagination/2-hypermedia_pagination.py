5aeca3d5b7348a8e2294dce99ea28b33cf38dfa0
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-07 10:52:45 +0000

    Add 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -64,8 +64,8 @@ class Server:
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
 
-        prev_page = page - 1 if page < total_pages else None
-        next_page = page + 1 if page > 1 else None
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
         
         return {
             "page_size": page_size,
