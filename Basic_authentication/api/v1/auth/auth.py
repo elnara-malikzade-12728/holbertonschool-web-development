848d59fbc4533d30be44fa2cb4eb096bf2ac8a39
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-04-09 21:14:02 +0400

    Add task3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+from flask import request
+from api.v1.auth import Auth
+
+
+class Auth():
+    """Class for authentication
+    """
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """A function for choosing the right paths
+        """
+        return false
+
+
+    def authorization_header(self, request=None) -> str:
+        """A function for authorization header
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """A function for user
+        """     
