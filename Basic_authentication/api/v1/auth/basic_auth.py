be7169168fe6d7a440cad42b46df9ba7afc9a598
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-10 11:08:50 +0000

    Add task9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,15 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """A function that returns the user email and
+        password from the Base64 decoded value
+        """
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str) or
+                ':' not in decoded_base64_authorization_header):
+            return (None, None)
+        parts = decoded_base64_authorization_header.split(':', 1)
+        return (parts[0], parts[1])
