de8ec25405de5b42e03f0d8033270f50104148d0
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-12 14:33:48 +0000

    Fix the snake case in session_auth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -20,7 +20,7 @@ if auth_type == 'auth':
 elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'session-auth':
+elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
