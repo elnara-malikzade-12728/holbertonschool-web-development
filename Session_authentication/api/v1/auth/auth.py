3b18c75fa8232cc4fbd96fc5d688ce114ad418a7
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-12 16:37:55 +0000

    Add task4

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
-
+import os
 
 class Auth():
     """Class for authentication
@@ -31,3 +31,13 @@ class Auth():
         """A function for user
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME')
+        
+        return request.cookies.get(session_name)
