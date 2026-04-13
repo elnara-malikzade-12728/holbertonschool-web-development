fe5ea5358cc18309644cb6213857ed99dd7fb4ff
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-13 18:44:13 +0000

    Add task8

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -39,3 +39,16 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout function
+    """
+    from api.v1.auth import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
