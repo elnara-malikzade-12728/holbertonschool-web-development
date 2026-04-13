7fe2026437d50efff76c4d502983e08f17e09af3
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-13 19:10:10 +0000

    Fix the import

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -32,7 +32,7 @@ def login():
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    from api.v1.app as import auth
+    from api.v1.app import auth
 
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
