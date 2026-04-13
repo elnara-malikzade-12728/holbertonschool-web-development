6bda2275058572799bd57d38adb99df6473ea744
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-13 17:32:41 +0000

    Add task7

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""A Flask view module 
+"""A Flask view module
 """
 from api.v1.views import app_views
 from flask import abort, request, jsonify
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -13,23 +14,23 @@ def login():
     password = request.form.get('password')
 
     if not email or email == "":
-        return jsonify("error": "email missing"), 400
+        return jsonify({"error": "email missing"}), 400
 
     if not password or password == "":
-        return jsonify("error": "password missing"), 400
- 
+        return jsonify({"error": "password missing"}), 400
+
     from models.user import User
     try:
-        users = User.search({"email": user_email})
+        users = User.search({"email": email})
     except Exception:
         return None
 
-    if  not users:
-        return jsonify("error": "no user found for this email"), 404
+    if not users or len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 401
+    user = users[0]
 
-    for user in users:
-        if not user.is_valid_password(password):
-            return jsonify("error": "wrong password"), 401
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
@@ -38,4 +39,3 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
-        
