6457c1910663c35eb2ee6898474b72309a8fae56
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-14 11:27:36 +0000

    Add task5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """A module for authentication
 """
 import bcrypt
+from db import DB
+from user import User, Base
 
 
 def _hash_password(password: str) -> str:
@@ -12,3 +14,23 @@ def _hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed.decode('utf-8')
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """takes mandatory email and password string arguments
+        and returns a User object.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists". format(email))
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+
+        return self._db.add_user(email, hashed_pwd)
