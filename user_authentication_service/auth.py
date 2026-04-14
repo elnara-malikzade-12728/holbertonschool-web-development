6c64bb54e0dabdfc192131a8f3b8dcd583b55f22
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-14 10:40:35 +0000

    Add task4

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """A module for authentication
 """
-import bcrypt
+from bcrypt import hashpw, gensalt
 
 
 def _hash_password(self, password: str) -> str:
     """A function to hash the given password and return a string
     """
-    hashed_bytes = bcrypt.hashpw(password.encode('utf-8'), gensalt())
+    hashed_bytes = hashpw(password.encode('utf-8'), gensalt())
     return hashed_bytes.decode('utf-8')
