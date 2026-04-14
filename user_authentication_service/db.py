3b67bf4dffd69582c8ab08a0fc3e85a5413b2716
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-14 09:18:46 +0000

    Fix db.py

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -26,7 +26,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> str:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Returns a User object
         """
         new_user = User(email=email, hashed_password=hashed_password)
