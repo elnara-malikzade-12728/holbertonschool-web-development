89252c3d98ad56c7392923453a28e336658c395f
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-14 13:13:33 +0000

    Fix pycodestyle error

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import User, Base
-from bcrypt import hashpw
+
 
 class DB:
     """Database class
