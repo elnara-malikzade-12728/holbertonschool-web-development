e0ae3dc34d6301e919738d4d1686273e4d81faef
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-15 17:29:14 +0000

    Add task13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,7 +85,7 @@ class Auth:
         """takes a single user_id integer argument and returns None
         """
         try:
-            user = self._db.find_user_by(user_id=user_id)
+            user = self._db.find_user_by(id=user_id)
         except NoResultFound:
             return None
         return self._db.update_user(user.id, session_id=None)
