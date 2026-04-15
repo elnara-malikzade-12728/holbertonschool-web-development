2d6716db3e6827a991a7ebfee43014112ef396be
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-15 17:17:16 +0000

    Add task13

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -80,3 +80,12 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """takes a single user_id integer argument and returns None
+        """
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+        except NoResultFound:
+            return None
+        return self._db.update_user(user.id, session_id=None)
