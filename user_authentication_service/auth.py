e4df7a9945d323fefbcf3bcee135cce006f6ae8b
Elnara Malikzade <12728@holbertonstudents.com>
2026-04-16 08:55:45 +0000

    Add task16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -93,8 +93,9 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """Takes an email string argument and returns a string.
         """
-        user = self._db.find_user_by(email=email)
-        if user is None:
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
